f3d5428c0f5b9b6158be63a6af54e6e67d79c6bc
Victor Candeira <vscandeira@gmail.com>
2024-05-16 15:04:16 -0300

    Teste de credenciais

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Internals
+    // Internals - teste
     [HideInInspector] public StateMachine stateMachine;
     [HideInInspector] public Idle idleState;
     [HideInInspector] public Walking walkingState;
